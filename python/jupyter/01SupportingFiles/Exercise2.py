ddc374b189d0af3ba04b689d6b8803ffdda50ea7
daverick <daverick@users.noreply.github.com>
2018-04-11 17:53:29 +0200

    Added the chapter 2: getting the birth year

---
+++ python/jupyter/01SupportingFiles/Exercise2.py
@@ -0,0 +1,5 @@
+first_word=input("What is the first word?")
+second_word=input("What is the second word?")
+third_word=input("What is the third word?")
+
+print(first_word,second_word,third_word,sep='🦄',end='🌈')
